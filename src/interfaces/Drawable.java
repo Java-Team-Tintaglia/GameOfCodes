8511ff631700532a6314cfd931e0a91f5b69212e
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-06-04 20:40:37 +0300

    Drawable and Updateble added

---
+++ src/interfaces/Drawable.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import java.awt.*;
+
+/**
+ * Created by PetyoPetrov on 04.06.2016 г..
+ */
+public interface Drawable {
+
+    void draw(Graphics graphics);
+}
---
+++ src/interfaces/Updatable.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+/**
+ * Created by PetyoPetrov on 04.06.2016 г..
+ */
+public interface Updatable {
+
+    void update();
+}
