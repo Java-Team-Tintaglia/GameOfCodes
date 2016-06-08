52966a76bb143c117da329162f5022db9e9727bd
sokito <sokratovgeorgi.w@gmail.com>
2016-06-08 12:37:48 +0300

    Class Student fixed
    added width and heigth properties, getters and setters

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,19 +1,16 @@
 package models;
 
-import com.sun.deploy.ui.ImageLoader;
 import graphics.SpriteSheet;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
-/**
- * Created by soki on 08-Jun-16.
- */
 public abstract class Student extends GameObject{
 
     private int x;
     private int y;
     SpriteSheet spriteSheet;
+    private int width;
+    private int height;
     private String name;
     private int intelligence;
     private int knowledge;
@@ -21,51 +18,80 @@ public abstract class Student extends GameObject{
     private Rectangle colliderBox;
 
 
-    public Student(int x , int y,SpriteSheet spriteSheet,String name, int intelligence, int knowledge, int vitality) {
+    public Student(int x , int y,SpriteSheet spriteSheet,int width, int height,String name, int intelligence, int knowledge, int vitality) {
         this.x = x;
         this.y = y;
         this.spriteSheet = spriteSheet;
+        this.width = width;
+        this.height = height;
         this.name = name;
         this.intelligence = intelligence;
         this.knowledge = knowledge;
         this.vitality = vitality;
-        this.colliderBox = new Rectangle(this.getX(), this.getY());
+        this.colliderBox = new Rectangle(this.getX(), this.getY(),this.width,this.height);
     }
 
 
     public int getX() {
         return x;
     }
+
     public void setX(int x) {
         this.x = x;
     }
+
     public int getY() {
         return y;
     }
+
     public void setY(int y) {
         this.y = y;
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getVitality(){
         return vitality;
     }
+
     public void setVitality(int vitality){
         this.vitality = vitality;
     }
+
     public int getKnowledge() {
         return knowledge;
     }
+
     public void setKnowledge(int knowledge) {
         this.knowledge = knowledge;
     }
+
     public int getIntelligence() {
         return intelligence;
     }
+
     public void setIntelligence(int intelligence) {
         this.intelligence = intelligence;
     }
