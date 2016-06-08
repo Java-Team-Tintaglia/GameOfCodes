25e93ea779f09e47bcade00d9136a360316737a6
tsvetelin-tsvetanov <tsvetelnitsvetanov94@gmail.com>
2016-06-09 00:02:12 +0300

    Added package repositories
    + class student-scores-repositories
    + file High-Scores in resources directory

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -3,4 +3,5 @@ package utils;
 public class Constants {
 	public static final int SCREEN_WIDTH = 1024;
 	public static final int SCREEN_HEIGHT = 600;
+	public static final String SAVE_FILE_PATH = "resources/High-Scores.txt";
 }
