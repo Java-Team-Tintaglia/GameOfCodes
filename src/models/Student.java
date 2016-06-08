da2f4ac6c065786342e2139ecd6f9d32b725324d
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-06-08 16:59:06 +0300

    some changes in class Student

--- src/models/Student.java
+++ src/models/Student.java
@@ -9,15 +9,66 @@ public abstract class Student extends GameObject{
     private String name;
     private int intelligence;
     private int knowledge;
-    private int vitality;
+    private int vitality=100;
 
-    public Student(int x, int y, int x1, int y1, String name, int intelligence, int knowledge, int vitality) {
+    public Student(int x, int y, String name, int intelligence, int knowledge, int vitality) {
         super(x, y);
-        x = x1;
-        y = y1;
+
         this.name = name;
         this.intelligence = intelligence;
         this.knowledge = knowledge;
+        this.vitality = vitality;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getKnowledge() {
+        return knowledge;
+    }
+
+    public void setKnowledge(int knowledge) {
+        this.knowledge = knowledge;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public void setVitality(int vitality) {
         this.vitality = vitality;
     }
 }
