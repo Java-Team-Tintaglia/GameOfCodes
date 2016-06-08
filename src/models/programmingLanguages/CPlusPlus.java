cd4ee93da968148471b8793721919bb20ffc5b67
Atanas Yankov <yankov1988@gmail.com>
2016-06-08 23:17:21 +0300

    Add Classes Java, Php, JavaScript, CPlusPlus

---
+++ src/models/programmingLanguages/CPlusPlus.java
@@ -0,0 +1,11 @@
+package models.programmingLanguages;
+
+import enums.ProgrammingLanguageType;
+
+public class CPlusPlus extends ProgrammingLanguage {
+
+    public CPlusPlus(int x, int y) {
+        super(x, y, ProgrammingLanguageType.JAVA, 10, 10);
+    }
+
+}
---
+++ src/models/programmingLanguages/Java.java
@@ -0,0 +1,10 @@
+package models.programmingLanguages;
+
+import enums.ProgrammingLanguageType;
+
+public class Java extends ProgrammingLanguage {
+
+    public Java(int x, int y) {
+        super(x, y, ProgrammingLanguageType.JAVA, 7, 7);
+    }
+}
