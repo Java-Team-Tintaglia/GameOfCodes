5e38b232bed9d1e6140e7c08b0c32e6e5e05ab36
Ivanmilev87 <Ivanmilev87@gmail.com>
2016-06-09 07:20:10 +0300

    2 new
    new classes badboy,hotChick

---
+++ src/models/students/HotChick.java
@@ -0,0 +1,11 @@
+package models.students;
+
+import graphics.Assets;
+
+import static graphics.Assets.hotChick;
+
+public class HotChick extends Student{
+    public HotChick(int x, int y, String name) {
+        super(x, y, hotChick, 0, 0, name, 80, 70, 100);
+    }
+}
