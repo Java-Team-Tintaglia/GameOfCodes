eef0033faaf6a59518d54c8abd9bd0ba7546fe83
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-06-09 23:23:37 +0300

    metod renderPlayer in GameState

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -2,21 +2,31 @@ package states;
 
 import java.awt.Graphics;
 
+import core.GameEngine;
+import eventhandler.MouseInput;
 import graphics.Assets;
+import graphics.Display;
 import utils.Constants;
 
 public class GameState extends State {
 
-	@Override
-	public void draw(Graphics graphics) {
-		graphics.drawImage(Assets.wall, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.drawImage(Assets.wall, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
         graphics.drawImage(Assets.floor, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
+        renderPlayer(graphics);
 
-		
-	}
 
-	@Override
-	public void update() {
-	}
-	
+    }
+
+    @Override
+    public void update() {
+    }
+
+    public void renderPlayer(Graphics graphics) {
+        
+
+    }
+
 }
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
 	public static final int SCREEN_WIDTH = 1024;
 	public static final int SCREEN_HEIGHT = 600;
 	public static final String SAVE_FILE_PATH = "resources/High-Scores.txt";
+	public static final int NERDBOY_WIDTH = 32;
+	public static final int NERDBOY_HEIGHT = 32;
+
 }
