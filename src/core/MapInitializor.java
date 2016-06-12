497d4042c2320612dbf97756a2181dbfe7a00dc8
mbeleva88 <m_d_b@mail.bg>
2016-06-12 23:16:37 +0300

    changed string to enum type

--- src/core/MapInitializor.java
+++ src/core/MapInitializor.java
@@ -1,33 +1,42 @@
 package core;
 
+import enums.ProgrammingLanguageType;
 import models.programmingLanguages.*;
 import utils.RandomGenerator;
 
 public class MapInitializor {
 
     public static ProgrammingLanguage generateProgrammingLanguage(){
-        String[] programmingLanguages = {"CPlusPlus", "CSharp", "Java", "JavaScript", "Php"};
+        ProgrammingLanguageType[] programmingLanguages = {
+        		ProgrammingLanguageType.JAVA,
+        		ProgrammingLanguageType.C_PLUS_PLUS, 
+        		ProgrammingLanguageType.C_SHARP,
+        		ProgrammingLanguageType.PHP,
+        		ProgrammingLanguageType.JAVA_SCRIPT};
+        
         int randomIndex = RandomGenerator.getNextRandom(programmingLanguages.length);
-        String randomLanguage = programmingLanguages[randomIndex];
+        
+        ProgrammingLanguageType randomLanguage = programmingLanguages[randomIndex];
+        
         int randomX = RandomGenerator.getNextRandom(800);
         int randomY = RandomGenerator.getNextRandom(300);
 
         ProgrammingLanguage langToBeCreated = null;
 
         switch (randomLanguage){
-            case "CPlusPlus":
+            case C_PLUS_PLUS:
                 langToBeCreated = new CPlusPlus(randomX, randomY);
                 break;
-            case "CSharp":
+            case C_SHARP:
                 langToBeCreated = new CSharp(randomX, randomY);
                 break;
-            case "Java":
+            case JAVA:
                 langToBeCreated = new Java(randomX, randomY);
                 break;
-            case "JavaScript":
+            case JAVA_SCRIPT:
                 langToBeCreated = new JavaScript(randomX, randomY);
                 break;
-            case "Php":
+            case PHP:
                 langToBeCreated = new Php(randomX, randomY);
                 break;
         }
