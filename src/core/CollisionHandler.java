3383787646de7f54881f9298df8be2fd3c18b9f2
mbeleva88 <m_d_b@mail.bg>
2016-06-13 09:52:08 +0300

    created list of programming languages + wizzard in gamestate

--- src/core/CollisionHandler.java
+++ src/core/CollisionHandler.java
@@ -1,24 +1,18 @@
 package core;
 
+import java.util.List;
 
-import models.GameObject;
 import models.programmingLanguages.ProgrammingLanguage;
 import models.students.Student;
 import models.wizards.Wizard;
-import states.GameState;
-
-import java.awt.*;
 
 public class CollisionHandler {
 
+    public static void collisionHandler(Student student, 
+    		Wizard wizard,
+    		List<ProgrammingLanguage> programmingLanguages) {
 
-    public void collisionHandler(){
-
-        for (GameObject entity : GameState.entities) {
-            // TODO: to foreach all the entities-they must have boundbox 
-         //   if(GameState.student.intersects())
-        }
-
+    	
     }
 
 
--- src/states/GameState.java
+++ src/states/GameState.java
@@ -6,29 +6,23 @@ import java.util.List;
 
 import factories.StudentFactory;
 import graphics.Assets;
-import models.GameObject;
 import models.programmingLanguages.*;
 import models.students.NerdBoy;
 import models.students.Student;
-import models.wizards.KnowledgeWizard;
 import models.wizards.Wizard;
 import utils.Constants;
-
-import static core.MapInitializor.generateProgrammingLanguage;
-import static core.MapInitializor.generateWizard;
+import core.CollisionHandler;
+import core.MapInitializor;
 
 public class GameState extends State {
-    public static List<GameObject> entities = new ArrayList<>();
-
+    private List<ProgrammingLanguage> programmingLanguages = new ArrayList<>();
+    private Wizard wizard;
+    private long timerNewWizard = System.nanoTime();
+	private long timerNewProgrammingLanguage = System.nanoTime();
+	private long timeDelayNewWizard = 1000;
+	private long timeDelayNewProgrammingLanguage = 500;
+    
     public static Student student = new NerdBoy(100, 100, "Misho");
-
-    public static ProgrammingLanguage java = new Java(200, 100);
-    public static ProgrammingLanguage javascript = new JavaScript(300, 100);
-    public static ProgrammingLanguage php = new Php(400, 100);
-    public static ProgrammingLanguage cplusplus = new CPlusPlus(500, 100);
-    public static ProgrammingLanguage csharp = new CSharp(600, 100);
-
-    public static Wizard wizard = new KnowledgeWizard(300, 300);
 
     StudentFactory studentFactory = new StudentFactory();
 
@@ -37,41 +31,53 @@ public class GameState extends State {
 //		
 //		student = studentFactory.create(studentType, x, y, name);
 //	}
+    
+    public GameState() {
+    	programmingLanguages.add(MapInitializor.generateProgrammingLanguage());
+    	wizard = MapInitializor.generateWizard();
+	}
 
     @Override
     public void draw(Graphics graphics) {
         graphics.drawImage(Assets.wall, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
-
         graphics.drawImage(Assets.floor, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
 
         student.draw(graphics);
-        java.draw(graphics);
-        javascript.draw(graphics);
-        cplusplus.draw(graphics);
-        csharp.draw(graphics);
-        php.draw(graphics);
-
-        wizard.draw(graphics);
-
-        //uncoment for test
-        //generateWizard().draw(graphics);
-        //generateProgrammingLanguage().draw(graphics);
-
+        
+        if (!programmingLanguages.isEmpty()) {
+        	 for (ProgrammingLanguage programmingLanguage : programmingLanguages) {
+             	programmingLanguage.draw(graphics);
+     		}
+		}
+       
+       
+        if (wizard != null) {
+        	wizard.draw(graphics);
+		}
+        
 
     }
 
     @Override
     public void update() {
-//pulnq gi rushno, zasega
-        entities.add(student);
-        entities.add(wizard);
+
+    	// timers not working, investigate why
+    	long elapsedNewWizard = (System.nanoTime() - this.timeDelayNewWizard) / 1000000;
+		long elapsedNewProgrammingLanguage = (System.nanoTime() - this.timeDelayNewProgrammingLanguage) / 1000000;
+    	
+    	CollisionHandler.collisionHandler(student, wizard, programmingLanguages);
+    	
+        if (elapsedNewWizard > this.timerNewWizard) {
+			wizard = MapInitializor.generateWizard();
+			timerNewWizard = System.nanoTime();
+		}
+    	
+        if (elapsedNewProgrammingLanguage > this.timerNewProgrammingLanguage) {
+			programmingLanguages.add(MapInitializor.generateProgrammingLanguage());
+			timerNewProgrammingLanguage = System.nanoTime();
+		}
+    	
         student.update();
-        java.update();
-        javascript.update();
-        php.update();
-        cplusplus.update();
-        csharp.update();
+
     }
-
-
 }
