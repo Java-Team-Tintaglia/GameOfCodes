843574512e18cb0a7b9d1e8309cb251aa1e47d4c
Rositca Koriykova <rosi.korikova@gmail.com>
2016-06-15 04:03:40 +0300

    changed diploma image and added draw method to StudentScoreState

--- src/states/StudentScoreState.java
+++ src/states/StudentScoreState.java
@@ -3,10 +3,41 @@ package states;
 
 import models.students.Student;
 
-public class StudentScoreState {
+import java.awt.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StudentScoreState extends State {
+
     private Student student;
 
     public StudentScoreState(Student student){
         this.student = student;
     }
+
+        @Override
+        public void draw(Graphics graphics) {
+            graphics.drawString(this.student.getName(),100,200);
+
+            int currentY = 100;
+            for (Map.Entry<String,java.util.List<Integer>> entry : this.student.getStudentGrades().entrySet()) {
+                Double grade = 0d;
+                int count = 0;
+                for (Integer s : entry.getValue()) {
+                    grade+= s;
+                    count++;
+                }
+                grade/= count;
+                graphics.drawString(entry.getKey(), 100, currentY);
+                graphics.drawString(grade.toString(), 200, currentY);
+                currentY+=100;
+            }
+        }
+
+        @Override
+        public void update() {
+
+        }
+
 }
+
