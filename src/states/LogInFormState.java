73bebbd3996e6edfcfad2248c28da6af4acf8304
Georgi Krumov <akageorgi@gmail.com>
2016-06-26 13:53:59 +0300

    LogIn Form

--- src/states/LogInFormState.java
+++ src/states/LogInFormState.java
@@ -9,16 +9,16 @@ import java.awt.*;
 
 public class LogInFormState extends State {
     public static StringBuilder username = new StringBuilder();
-    public static StringBuilder firstName = new StringBuilder();
-    public static StringBuilder lastName = new StringBuilder();
+//    public static StringBuilder firstName = new StringBuilder();
+//    public static StringBuilder lastName = new StringBuilder();
     public static StringBuilder password = new StringBuilder();
 
     public static Button backToMenuButton = new Button(712, 510, Assets.buttonBackToMenu);
     public static Button loginButton = new Button(496, 510, Assets.buttonLogIn);//TO DO change x and y
 
     public static Rectangle userRect;
-    public static Rectangle firstRect;
-    public static Rectangle lastRect;
+//    public static Rectangle firstRect;
+//    public static Rectangle lastRect;
     public static Rectangle passRect;
 
     public static boolean isFieldSelected = false;
@@ -47,30 +47,18 @@ public class LogInFormState extends State {
         Font fieldName = new Font("Arial", Font.PLAIN, 25);
         graphics.setFont(fieldName);
         graphics.drawString("Username:", fieldNameX, fieldNameY);
-        graphics.drawString("First Name:", fieldNameX, fieldNameY + 60);
-        graphics.drawString("Last Name:", fieldNameX, fieldNameY + 120);
-        graphics.drawString("Password:", fieldNameX, fieldNameY + 180);
+        graphics.drawString("Password:", fieldNameX, fieldNameY + 60);
 
         graphics.fillRect(rectBoxX, rectBoxY, 300, 40);
         userRect = new Rectangle(rectBoxX, rectBoxY, 300, 40);
         graphics.fillRect(rectBoxX, rectBoxY + 60, 300, 40);
-        firstRect = new Rectangle(rectBoxX, rectBoxY + 60, 300, 40);
-        graphics.fillRect(rectBoxX, rectBoxY + 120, 300, 40);
-        lastRect = new Rectangle(rectBoxX, rectBoxY + 120, 300, 40);
-        graphics.fillRect(rectBoxX, rectBoxY + 180, 300, 40);
-        passRect = new Rectangle(rectBoxX, rectBoxY + 180, 300, 40);
+        passRect = new Rectangle(rectBoxX, rectBoxY + 60, 300, 40);
 
         graphics.setColor(Color.gray);
         if (isFieldSelected) {
             switch (fieldType) {
                 case "user":
                     graphics.drawRect((int)userRect.getX() - 3, (int)userRect.getY() - 3, 306, 46);
-                    break;
-                case "first":
-                    graphics.drawRect((int)firstRect.getX() - 3, (int)firstRect.getY() - 3, 306, 46);
-                    break;
-                case "last":
-                    graphics.drawRect((int)lastRect.getX() - 3, (int)lastRect.getY() - 3, 306, 46);
                     break;
                 case "pass":
                     graphics.drawRect((int)passRect.getX() - 3, (int)passRect.getY() - 3, 306, 46);
@@ -82,9 +70,7 @@ public class LogInFormState extends State {
         graphics.setFont(inputText);
         graphics.setColor(Color.black);
         graphics.drawString(username.toString(), rectBoxX + 30, rectBoxY + 25);
-        graphics.drawString(firstName.toString(), rectBoxX + 30, rectBoxY + 85);
-        graphics.drawString(lastName.toString(), rectBoxX + 30, rectBoxY + 145);
-        graphics.drawString(password.toString(), rectBoxX + 30, rectBoxY + 205);
+        graphics.drawString(password.toString(), rectBoxX + 30, rectBoxY + 85);
 
         backToMenuButton.draw(graphics);
         loginButton.draw(graphics);
