c3f8f04cfdf2a017304092e504063b1a71e9efac
tsvetelin-tsvetanov <tsvetelnitsvetanov94@gmail.com>
2016-06-27 14:41:39 +0300

    fixed Error and Success Message States

--- src/states/ErrorMessageState.java
+++ src/states/ErrorMessageState.java
@@ -1,9 +1,16 @@
 package states;
 
+import graphics.Assets;
+import models.Button;
+import utils.Constants;
+
 import java.awt.*;
+import java.util.StringTokenizer;
 
 public class ErrorMessageState extends State{
     private String message;
+
+    public static Button okButton = new Button(400, 310, Assets.buttonOk);
 
     public ErrorMessageState(String message) {
         this.message = message;
@@ -11,13 +18,35 @@ public class ErrorMessageState extends State{
 
     @Override
     public void draw(Graphics graphics) {
-        int messagePositionX = 650;
-        int messagePositionY = 250;
+        int messagePositionX = 270;
+        int messagePositionY = 190;
 
-        Font font = new Font("Arial", Font.PLAIN, 35);
-        graphics.setFont(font);
+        graphics.drawImage(Assets.wall, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
+        graphics.fillRect(100, 40, 824, 450);
+
+        Font titleFont = new Font("Arial", Font.BOLD, 35);
+        graphics.setFont(titleFont);
         graphics.setColor(Color.red);
-        graphics.drawString(this.message, messagePositionX, messagePositionY);
+        graphics.drawString("Error", 370, 90);
+
+        StringTokenizer tokens = new StringTokenizer(this.message, " ");
+        StringBuilder output = new StringBuilder(this.message.length());
+        int lineLen = 0;
+        while (tokens.hasMoreTokens()) {
+            String word = tokens.nextToken();
+
+            if (lineLen + word.length() > 23) {
+                output.append("\n");
+                lineLen = 0;
+            }
+            output.append(word + " ");
+            lineLen += word.length();
+        }
+
+        graphics.setFont(new Font("Arial", Font.PLAIN, 25));
+        graphics.drawString(output.toString(), messagePositionX, messagePositionY);
+
+        okButton.draw(graphics);
     }
 
     @Override
--- src/states/SuccessMessageState.java
+++ src/states/SuccessMessageState.java
@@ -1,9 +1,15 @@
 package states;
 
+import graphics.Assets;
+import utils.Constants;
+
 import java.awt.*;
+import java.util.StringTokenizer;
 
 public class SuccessMessageState extends State{
     private String message;
+
+    public static models.Button okButton = new models.Button(400, 310, Assets.buttonOk);
 
     public SuccessMessageState(String message) {
         this.message = message;
@@ -11,13 +17,36 @@ public class SuccessMessageState extends State{
 
     @Override
     public void draw(Graphics graphics) {
-        int messagePositionX = 650;
-        int messagePositionY = 250;
+        int messagePositionX = 270;
+        int messagePositionY = 190;
 
-        Font font = new Font("Arial", Font.PLAIN, 35);
-        graphics.setFont(font);
+        graphics.drawImage(Assets.wall, 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, null);
+        graphics.fillRect(100, 40, 824, 450);
+
+        Font titleFont = new Font("Arial", Font.BOLD, 35);
+        graphics.setFont(titleFont);
         graphics.setColor(Color.green);
-        graphics.drawString(this.message, messagePositionX, messagePositionY);
+        graphics.drawString("Success", 370, 90);
+
+        StringTokenizer tokens = new StringTokenizer(this.message, " ");
+        StringBuilder output = new StringBuilder(this.message.length());
+        int lineLen = 0;
+        while (tokens.hasMoreTokens()) {
+            String word = tokens.nextToken();
+
+            if (lineLen + word.length() > 23) {
+                output.append("\n");
+                lineLen = 0;
+            }
+            output.append(word + " ");
+            lineLen += word.length();
+        }
+
+        Font messageFont = new Font("Arial", Font.PLAIN, 25);
+        graphics.setFont(messageFont);
+        graphics.drawString(output.toString(), messagePositionX, messagePositionY);
+
+        okButton.draw(graphics);
     }
 
     @Override
