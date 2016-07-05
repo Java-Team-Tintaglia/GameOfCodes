17d5bd0f2f45f034f2cb18a9eba0f8b2ed64cf34
Atanas Yankov <yankov1988@gmail.com>
2016-07-05 16:42:29 +0300

    bug fixed

--- src/states/StudentProfileState.java
+++ src/states/StudentProfileState.java
@@ -2,19 +2,16 @@ package states;
 
 import authentication.AuthenticationProvider;
 import graphics.Assets;
-import models.*;
 import models.Button;
 import repositories.StudentScoresRepository;
 import utils.Constants;
-import utils.Utils;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class StudentProfileState extends State {
+
     public static StringBuilder firstName = new StringBuilder(
             AuthenticationProvider.currentUser.getFirstName());
 
@@ -28,16 +25,11 @@ public class StudentProfileState extends State {
             StudentScoresRepository.getAllGrades(AuthenticationProvider.currentUser.getFirstName()));
 
 
-
-    double averageGradeCSharp = grades.get("C#").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
-    double averageJava = grades.get("Java").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
-    double averageCPlusPlus = grades.get("C++").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
-    double averageJavaScript = grades.get("JavaScript").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
-    double averagePhp = grades.get("PHP").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
-
-
-
-
+    public double averageGradeCSharp = 0.0;
+    public double averageJava = 0.0;
+    public double averageCPlusPlus = 0.0;
+    public double averageJavaScript = 0.0;
+    public double averagePhp = 0.0;
 
 
     @Override
@@ -65,30 +57,30 @@ public class StudentProfileState extends State {
         //Grades
         graphics.drawString("GRADES", fieldNameX + 250, fieldNameY + 60);
 
-        graphics.drawString("C#:               " +  String.format("%.1f", averageGradeCSharp)  + " " , fieldNameX, fieldNameY + 80);
-        graphics.drawString("Java:            " +  String.format("%.1f", averageJava) + " " , fieldNameX, fieldNameY + 110);
-        graphics.drawString("C++:             " +  String.format("%.1f", averageCPlusPlus) + " " , fieldNameX, fieldNameY + 140);
-        graphics.drawString("JavaScript:   " +  String.format("%.1f", averageJavaScript) + " " , fieldNameX, fieldNameY + 170);
-        graphics.drawString("PHP:            " +  String.format("%.1f", averagePhp) + " " , fieldNameX, fieldNameY + 200);
+        graphics.drawString("C#:               " + String.format("%.1f", averageGradeCSharp) + " ", fieldNameX, fieldNameY + 80);
+        graphics.drawString("Java:            " + String.format("%.1f", averageJava) + " ", fieldNameX, fieldNameY + 110);
+        graphics.drawString("C++:             " + String.format("%.1f", averageCPlusPlus) + " ", fieldNameX, fieldNameY + 140);
+        graphics.drawString("JavaScript:   " + String.format("%.1f", averageJavaScript) + " ", fieldNameX, fieldNameY + 170);
+        graphics.drawString("PHP:            " + String.format("%.1f", averagePhp) + " ", fieldNameX, fieldNameY + 200);
 
         graphics.setColor(Color.gray);
 
-
-
-        //Font inputText = new Font("Arial", Font.BOLD, 20);
-        //graphics.setFont(inputText);
-        //graphics.setColor(Color.black);
-
-
         backToMenuButton.draw(graphics);
         editButton.draw(graphics);
-
 
 
     }
 
     @Override
     public void update() {
-
+        try {
+            averageGradeCSharp = grades.get("C#").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
+            averageJava = grades.get("Java").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
+            averageCPlusPlus = grades.get("C++").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
+            averageJavaScript = grades.get("JavaScript").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
+            averagePhp = grades.get("PHP").stream().mapToDouble(s -> s.intValue()).average().getAsDouble();
+        } catch (NullPointerException e) {
+        }
     }
-  }
+
+}
