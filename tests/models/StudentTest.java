c3a7f5d350cb3e1b5c4f82f71afb90eb9fc3d71a
Ignat Dishliev <i.dishliev87@gmail.com>
2016-08-03 12:48:25 +0300

    Student constructor unit tests

--- tests/models/StudentTest.java
+++ tests/models/StudentTest.java
@@ -1,17 +1,67 @@
 package models;
 
+import interfaces.Student;
+import models.students.BadBoy;
+import models.students.HotChick;
+import models.students.NerdBoy;
+import models.students.NerdLady;
 import org.junit.Assert;
 import org.junit.Test;
 
-import interfaces.Student;
-import models.students.BadBoy;
-
 public class StudentTest {
 
-	@Test
-	public void ConstructorStudentDifferentThanNull() {
-		Student student = new BadBoy(100, 100, "TEST", "TEST_TEST");
-		Assert.assertNotNull(student);
-	}
+    private static final String USERNAME = "testUsername";
+    private static final String FULL_NAME = "testFullName";
+    private static final int TEST_X = 100;
+    private static final int TEST_Y = 100;
 
+    //region Constructor Tests
+	@Test
+    public void testBadBoyConstructorReturnsCorrectStudent() {
+	    Student student = new BadBoy(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertEquals(BadBoy.class, student.getClass());
+    }
+
+    @Test
+    public void testBadBoyConstructorDifferentThanNull() {
+        Student student = new BadBoy(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertNotNull(student);
+    }
+
+    @Test
+    public void testHotChickConstructorReturnsCorrectStudent() {
+        Student student = new HotChick(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertEquals(HotChick.class, student.getClass());
+    }
+
+    @Test
+    public void testHotChickConstructorDifferentThanNull() {
+        Student student = new HotChick(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertNotNull(student);
+    }
+
+    @Test
+    public void testNerdBoyConstructorReturnsCorrectStudent() {
+        Student student = new NerdBoy(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertEquals(NerdBoy.class, student.getClass());
+    }
+
+    @Test
+    public void testNerdBoyConstructorDifferentThanNull() {
+        Student student = new NerdBoy(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertNotNull(student);
+    }
+
+    @Test
+    public void testNerdLadyConstructorReturnsCorrectStudent() {
+        Student student = new NerdLady(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertEquals(NerdLady.class, student.getClass());
+    }
+
+    @Test
+    public void testNerdLadyConstructorDifferentThanNull() {
+        Student student = new NerdLady(TEST_X, TEST_Y, USERNAME, FULL_NAME);
+        Assert.assertNotNull(student);
+    }
+    //endregion
 }
