59cb45d9a90f4663906ad19a01a5f5b5a7a4e56e
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-08-03 14:41:02 +0300

    correction to Unit test

--- tests/models/WizardTest.java
+++ tests/models/WizardTest.java
@@ -16,67 +16,61 @@ public class WizardTest {
 
     private static final int TEST_X_COORD = 100;
     private static final int TEST_Y_COORD = 100;
-    private static final int TEST_INTELIGENCE = 65;
-    private static final int EXPECTED_BADBOY_KNOWLEDGE = 75;
-    private static final int EXPECTED_BADBOY_VITALITY = 90;
-    private static final int ACTUAL_BADBOY_VITALITY = 60;
-    private Wizard intelligenceWizard;
-    private Wizard knowledgeWizard;
-    private Wizard vitalityWizard;
-    private Student student;
 
 
     @Before
     public void setUp() {
         Assets.init();
-        this.student = new BadBoy(TEST_X_COORD, TEST_Y_COORD, "TEST", "TEST");
-        this.intelligenceWizard = new IntelligenceWizard(TEST_X_COORD, TEST_Y_COORD);
-        this.knowledgeWizard=new KnowledgeWizard(TEST_X_COORD,TEST_Y_COORD);
-        this.vitalityWizard=new VitalityWizard(TEST_X_COORD,TEST_Y_COORD);
 
     }
-
 
     @Test
     public void constructorIntelligenceWizard_Check_Correctness() {
-        this.intelligenceWizard = new IntelligenceWizard(TEST_X_COORD, TEST_Y_COORD);
-        Assert.assertNotNull(this.intelligenceWizard);
+       Wizard intelligenceWizard = new IntelligenceWizard(TEST_X_COORD, TEST_Y_COORD);
+        Assert.assertNotNull(intelligenceWizard);
     }
+
     @Test
     public void constructorKnowledgeWizard_Check_Correctness() {
-        this.knowledgeWizard = new KnowledgeWizard(TEST_X_COORD, TEST_Y_COORD);
-        Assert.assertNotNull(this.knowledgeWizard);
+        Wizard knowledgeWizard = new KnowledgeWizard(TEST_X_COORD, TEST_Y_COORD);
+        Assert.assertNotNull(knowledgeWizard);
     }
+
     @Test
     public void constructorVitalityWizard_Check_Correctness() {
-        this.vitalityWizard = new VitalityWizard(TEST_X_COORD, TEST_Y_COORD);
-        Assert.assertNotNull(this.vitalityWizard);
+        Wizard vitalityWizard = new VitalityWizard(TEST_X_COORD, TEST_Y_COORD);
+        Assert.assertNotNull(vitalityWizard);
     }
 
-
-    //
     @Test
-    public void testBoost_Intelligence() throws Exception {
-        int expectedBedBoyIntelligence = TEST_INTELIGENCE;
-        this.intelligenceWizard.boost(this.student);
-        int actual = this.student.getIntelligence();
+    public void testBoost_Intelligence()  {
+        Wizard intelligenceWizard = new IntelligenceWizard(TEST_X_COORD, TEST_Y_COORD);
+        Student student=new BadBoy(TEST_X_COORD,TEST_Y_COORD,"TEST","TEST");
+        int expectedBedBoyIntelligence = 65;
+        intelligenceWizard.boost(student);
+        int actual = student.getIntelligence();
         Assert.assertEquals(expectedBedBoyIntelligence, actual);
 
     }
+
     @Test
-    public  void testBoost_Knowledge(){
-        int expectedBadBoyKnowledge = EXPECTED_BADBOY_KNOWLEDGE;
-        this.knowledgeWizard.boost(this.student);
-        int actual = this.student.getKnowledge();
+    public void testBoost_Knowledge() {
+        Wizard  knowledgeWizard= new KnowledgeWizard(TEST_X_COORD, TEST_Y_COORD);
+        Student student=new BadBoy(TEST_X_COORD,TEST_Y_COORD,"TEST","TEST");
+        int expectedBadBoyKnowledge =75;
+        knowledgeWizard.boost(student);
+        int actual = student.getKnowledge();
         Assert.assertEquals(expectedBadBoyKnowledge, actual);
     }
 
     @Test
-    public  void testBoost_Vitality(){
-        int expectedVitality=EXPECTED_BADBOY_VITALITY;
-        this.student.setVitality(ACTUAL_BADBOY_VITALITY);
-        this.vitalityWizard.boost(this.student);
-        int actual=this.student.getVitality();
+    public void testBoost_Vitality() {
+        Wizard  vitalityWizard= new VitalityWizard(TEST_X_COORD, TEST_Y_COORD);
+        Student student=new BadBoy(TEST_X_COORD,TEST_Y_COORD,"TEST","TEST");
+        int expectedVitality =90;
+        student.setVitality(60);
+        vitalityWizard.boost(student);
+        int actual = student.getVitality();
         Assert.assertEquals(expectedVitality, actual);
     }
 }
