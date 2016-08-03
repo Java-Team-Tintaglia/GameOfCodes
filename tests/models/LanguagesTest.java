2db77dc7ef50868aaccbc03b86b40570a1ee7980
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-08-03 19:05:52 +0300

    Unit test Languages

---
+++ tests/models/LanguagesTest.java
@@ -0,0 +1,47 @@
+package models;
+
+import graphics.Assets;
+import interfaces.ProgrammingLanguage;
+import models.programmingLanguages.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by PetyoPetrov on 03.08.2016 г..
+ */
+public class LanguagesTest {
+    private static final int X_COORD = 100;
+    private static final int Y_COORD = 100;
+
+    @Before
+    public void setUp(){
+        Assets.init();
+    }
+    @Test
+    public  void constructorCPlusPlusShouldNotBeNull(){
+        ProgrammingLanguage language=new CPlusPlus(X_COORD,Y_COORD);
+        Assert.assertNotNull(language);
+    }
+
+    @Test
+    public  void constructorCSharpShouldNotBeNull(){
+        ProgrammingLanguage language=new CSharp(X_COORD,Y_COORD);
+        Assert.assertNotNull(language);
+    }
+    @Test
+    public  void constructorJavaShouldNotBeNull(){
+        ProgrammingLanguage language=new Java(X_COORD,Y_COORD);
+        Assert.assertNotNull(language);
+    }
+    @Test
+    public  void constructorJavaScriptShouldNotBeNull(){
+        ProgrammingLanguage language=new JavaScript(X_COORD,Y_COORD);
+        Assert.assertNotNull(language);
+    }
+    @Test
+    public  void constructorPhpShouldNotBeNull(){
+        ProgrammingLanguage language=new Php(X_COORD,Y_COORD);
+        Assert.assertNotNull(language);
+    }
+}
